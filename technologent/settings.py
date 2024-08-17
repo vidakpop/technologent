983712ab2b4c494ed235c3f5e747a7c82a008db8
DAVIS <davis@kali>
2024-08-17 12:22:22 +0300

    add railway subdomain

--- technologent/settings.py
+++ technologent/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY =os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['technologent-shop.up.railway.app','https://technologent-shop.up.railway.app']
+CSRF_TRUSTED_ORIGINS=['https://technologent-shop.up.railway.app']
 
 
 # Application definition
