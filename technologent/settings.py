c27f9f2dbb420ae2828e2bd39a0eb28686504f43
DAVIS <davis@kali>
2024-08-17 12:38:27 +0300

    fixed port

--- technologent/settings.py
+++ technologent/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY =os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['technologent-shop.up.railway.app','https://technologent-shop.up.railway.app']
+ALLOWED_HOSTS = ['technologent-shop.up.railway.app','127.0.0.1','https://technologent-shop.up.railway.app']
 CSRF_TRUSTED_ORIGINS=['https://technologent-shop.up.railway.app']
 
 
